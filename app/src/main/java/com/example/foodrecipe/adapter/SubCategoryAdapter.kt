b9253b477a1b7dc37e228d13ec495219132e4b7c
Amir Salimi <108394058+TheSalimi@users.noreply.github.com>
2022-09-17 20:00:33 +0430

    Create SubCategoryAdapter.kt

---
+++ app/src/main/java/com/example/foodrecipe/adapter/SubCategoryAdapter.kt
@@ -0,0 +1,35 @@
+package com.example.foodrecipe.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.foodrecipe.R
+import com.example.foodrecipe.entities.Recipes
+import kotlinx.android.synthetic.main.item_rv_sub_category.view.*
+
+class SubCategoryAdapter : RecyclerView.Adapter<SubCategoryAdapter.RecipeViewHolder>() {
+    var arrSubCategory = ArrayList<Recipes>()
+    fun setData(arrData: List<Recipes>) {
+        arrSubCategory =
+            arrData as ArrayList<Recipes> /* = java.util.ArrayList<com.example.foodrecipe.entities.Recipes> */
+    }
+
+    class RecipeViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecipeViewHolder {
+        return RecipeViewHolder(
+            LayoutInflater.from(parent.context)
+                .inflate(R.layout.item_rv_sub_category, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: RecipeViewHolder, position: Int) {
+        holder.itemView.tv_dish_name.text = arrSubCategory[position].dishName
+    }
+
+    override fun getItemCount(): Int {
+        return arrSubCategory.size
+    }
+}
